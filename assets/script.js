fba97d8791f3ac4c2971428e9b0211f2c8f4a8bb
William <110736589+ConnorMurphy9@users.noreply.github.com>
2022-09-21 01:05:37 -0400

    added function to make buttons

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,76 @@ var timerElement = document.querySelector(".time");
 
 // Selects element by id
 var mainElement = document.getElementById("main");
+var startBtn = document.getElementById("startButton");
+var presentQuestion = document.getElementById("question");
+
+
+
+// var answerset1 = document.getElementById("button1");
+// var answerset2 = document.getElementById("button2");
+// var answerset3 = document.getElementById("button3");
+// var answerset4 = document.getElementById("button4");
+// var answerset5 = document.getElementById("button5");
+
+var allQuestions = [
+  
+// allQuestions[0].question;
+// allQuestions[0].choices;
+
+// 2
+{
+question: "Commonly used data types DO NOT include:", 
+choices: "string","booleans","alerts","numbers"},
+// 2
+{
+question: "The condition in a if/else statement in enclosed within:",
+choices: "quotes", "curly brackets", "parentheses", "square brackets"},
+// 3
+{
+question: "Arrays in Javascript can be used to store:", 
+choices: "numbers and strings", "other arrays", "booleans", "all of the above"},
+// 2
+{
+question: "String values must be enclosed within __ when being assigned to variables.", 
+choices: "commas", "curly brackets", "quotes", "parentheses"},
+// 3
+{
+question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+choices: "Javacript", "terminal/bash", "for loop", "console log"}
+];
+
+
+
+
+function makeButton() {
+allQuestions[0].choices.forEach(element=> { 
+  let btn = document.createElement('button');
+  btn.innerHTML = element;
+  document.getElementById(container).appendChild(btn);
+});}
+
+makeButton();
+
+
+
+console.log(question1);
+console.log(question2);
+console.log(question3);
+console.log(question4);
+console.log(question5);
+
+
+
+
+
+// function for wrong answer?
+// function wrongAnswer() {}
+
+function askQuestion() {
+
+
+
+}
 
 var secondsLeft = 75;
 
@@ -11,16 +81,31 @@ var secondsLeft = 75;
     
 // }
 
-
-function setCorrect() {
+// Function that will update the user's high score
+function updateCorrect() {
+    
     win.textContent = winCounter;
     localStorage.setItem("winCount", winCounter);
   }
 
+startBtn.addEventListener("click", startTimer); 
+
+
+function startTimer() {
+   
+
+  
+    startBtn.disabled = true;
+    // startBtn.style.display = "none"; 
+    timer();
+}
+
+
+
 
 
 // This is the timer.
-function Timer() {
+function timer() {
   // Sets interval in variable
   var timerInterval = setInterval(function() {
     secondsLeft--;
@@ -36,7 +121,7 @@ function Timer() {
   }, 1000);
 }
 
-  Timer();
+ 
   
 
 
@@ -50,10 +135,3 @@ function Timer() {
 
 
 
-
-// Answers to questions
-    //    question 1 </p>
-    // <p>question 2 quotes, curly brackets, parentheses, square brackets</p>
-    // <p>question 3 numbers and strings, other arrays, booleans, All of the above.</p>
-    // <p>question 4 commas, quotations, curly brackets, parentheses. </p>
-    // <p>question 5 JavaScript, terminal/bash, for loop, console log 
