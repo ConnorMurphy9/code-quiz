a82e016eb3724da7ca94581dbea28463dba79b55
William <110736589+ConnorMurphy9@users.noreply.github.com>
2022-09-24 13:06:06 -0400

    Added text to readme, deleted old comments

--- assets/script.js
+++ assets/script.js
@@ -128,9 +128,6 @@ function storeScore() {
   if (localStorage.getItem("userData") == null){
     localStorage.setItem("userData", []);
   }
-  // else {userData = JSON.parse(localStorage.getItem())}
-// var prevData = JSON.parse(localStorage.getItem('userData'));
-// prevData.push(userData);
 
 localStorage.setItem('userData', JSON.stringify(userData));
 var displayScore = JSON.parse(localStorage.getItem(userData));
@@ -144,34 +141,3 @@ userData.push(userData);
 
 };
 
-
-// function saveLastGrade() {
-//   // Save related form data as an object
-//   var studentGrade = {
-//     student: student.value,
-//     grade: grade.value,
-//     comment: comment.value.trim()
-//   };
-//   // Use .setItem() to store object in storage and JSON.stringify to convert it as a string
-//   localStorage.setItem("studentGrade", JSON.stringify(studentGrade));
-// }
-
-
-// function renderLastGrade() {
-//   // Use JSON.parse() to convert text to JavaScript object
-//   var lastGrade = JSON.parse(localStorage.getItem("studentGrade"));
-//   // Check if data is returned, if not exit out of the function
-//   if (lastGrade !== null) {
-//   document.getElementById("saved-name").innerHTML = lastGrade.student;
-//   document.getElementById("saved-grade").innerHTML = lastGrade.grade;
-//   document.getElementById("saved-comment").innerHTML = lastGrade.comment;
-//   } else {
-//     return;
-//   }
-// }
-
-submitButton.addEventListener("click", function(event) {
-  event.preventDefault();
-  saveLastGrade();
-  renderLastGrade();
-  });
